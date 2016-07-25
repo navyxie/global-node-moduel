74230d80139e6f91a8d563ecce75da900a0260b9
navyxie <navyxie2010@gmail.com>
2016-07-25 19:24:51 +0800

    修正命令行

--- bin/global-node-moduel.js
+++ bin/global-node-moduel.js
@@ -10,36 +10,39 @@ program.version(pkg.version)
     .option('-o, --output [path]', 'output file path')
     .option('-r, --remove [path]', 'remove file sync')
     .parse(process.argv);
-if (!program.args.length) {
-  console.log(chalk.blue.bold('[version]: ') + pkg.version);
-  return false;
-}
-var GMI = new GM();
-program.args.forEach(function () {
+
+init();
+function init() {
     var copyPath = program.copy;
     var outputPath = program.output;
     var removePath = program.remove;
-    if (copyPath) {
-        if (outputPath) {
-            try{
-                GMI.copySync(copyPath, outputPath);
-                console.log('copy file from ', path.resolve(copyPath), ' to ', path.resolve(outputPath));
-            } catch(e) {
-                console.log(chalk.red.bold('[copySync ERROR]: '+ e.message));
+    var GMI = new GM();
+    if (!copyPath || !removePath) {
+        console.log(chalk.blue.bold('[version]: ') + pkg.version);
+        program.help();
+    } else {
+        if (copyPath) {
+            if (outputPath) {
+                try{
+                    GMI.copySync(copyPath, outputPath);
+                    console.log('copy file from ', path.resolve(copyPath), ' to ', path.resolve(outputPath));
+                } catch(e) {
+                    console.log(chalk.red.bold('[copySync ERROR]: '+ e.message));
+                }
+                return;
+            } else {
+                console.log(chalk.yellow.bold('[miss param output]'));
+                return; 
             }
-            return;
-        } else {
-            console.log(chalk.yellow.bold('[miss param output]'));
-            return; 
+        }
+        if (removePath) {
+            try{
+                GMI.removeSync(removePath);
+                console.log('remove file from ', path.resolve(copyPath));
+            } catch(e) {
+                console.log(chalk.red.bold('[removeSync ERROR]: '+ e.message));
+            }
+            return;''
         }
     }
-    if (removePath) {
-        try{
-            GMI.removeSync(removePath);
-            console.log('remove file from ', path.resolve(copyPath));
-        } catch(e) {
-            console.log(chalk.red.bold('[removeSync ERROR]: '+ e.message));
-        }
-        return;''
-    }
-})
+}
